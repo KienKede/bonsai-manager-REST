64e1cfb1993ad822dd5ba68a09ef938c9425731f
KienKede <jordi.diu.r@gmail.com>
2017-06-21 18:11:21 +0200

    First Commit
    Project Structure

---
+++ src/main/java/com/bonsaimanager/bonsaimanager/BonsaiManagerApplication.java
@@ -0,0 +1,12 @@
+package com.bonsaimanager.bonsaimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonsaiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonsaiManagerApplication.class, args);
+	}
+}
